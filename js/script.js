76800f54b4c6874c79d89c2c86534242c6bc9327
Davlatov Muhriddin <162937360+davlatov-muhriddin1@users.noreply.github.com>
2025-08-25 08:59:55 +0500

    add reviews and faq section with responsive style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ============================================ NAVBAR ============================================
+
 const overlay = document.querySelector(".overlay");
 const navList = document.querySelector(".nav-list");
 const menuBtn = document.querySelector(".nav-menu");
@@ -15,4 +17,28 @@ menuBtn.addEventListener("click", () => {
   } else {
     menuBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
   }
+});
+
+// ============================================ ACCORDION ============================================
+
+const accordionContents = document.querySelectorAll(".faq-item-content");
+const accordionIcons = document.querySelectorAll(".faq-item button");
+
+let accordionToggle = false;
+
+accordionIcons.forEach((accIcon, accIconIndex) => {
+  accIcon.addEventListener("click", () => {
+    accordionToggle = !accordionToggle;
+    if (accordionToggle) {
+      accIcon.innerHTML = `<i class="fa-solid fa-chevron-up"></i>`;
+    } else {
+      accIcon.innerHTML = `<i class="fa-solid fa-chevron-down"></i>`;
+    }
+
+    accordionContents.forEach((accItem, accItemIndex) => {
+      if (accIconIndex === accItemIndex) {
+        accItem.classList.toggle("show");
+      }
+    });
+  });
 });
